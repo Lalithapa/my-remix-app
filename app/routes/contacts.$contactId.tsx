da1b63ca6a721ae56cbaeaf346cf4150be201515
Lalit Thapa <lalithapaji@gmail.com>
2024-10-01 18:22:51 +0530

    Created contacts Page

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -20,15 +20,6 @@ export const loader = async ({
 
 export default function Contact() {
   const { contact } = useLoaderData<typeof loader>();
-  // const contact = {
-  //   first: "Your",
-  //   last: "Name",
-  //   avatar: "https://placecats.com/200/200",
-  //   twitter: "your_handle",
-  //   notes: "Some notes",
-  //   favorite: true,
-  // };
-
   return (
     <div id="contact">
       <div>
